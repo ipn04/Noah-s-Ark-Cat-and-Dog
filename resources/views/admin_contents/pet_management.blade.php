60be10e15428e59da38dc0ee6554e7897b80e08e
ivan <ivan.nicolas446@gmail.com>
2023-11-25 20:38:50 +0800

    second commit

--- resources/views/admin_contents/pet_management.blade.php
+++ resources/views/admin_contents/pet_management.blade.php
@@ -815,10 +815,12 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                        @foreach($pets as $pet)
+                        @if($pets->isNotEmpty())
                             <button data-modal-toggle="delete-modal" type="button" onclick="deletePet('{{ $pet->id }}')" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Yes, I'm sure</button>
-                        @endforeach
-                        
+                        @else
+                            <!-- Handle case when no pets are available -->
+                            <p>No pets available for deletion.</p>
+                        @endif
                         <button data-modal-toggle="delete-modal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
                     </div>
                 </div>

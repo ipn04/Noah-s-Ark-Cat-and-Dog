1d3a9b645fbec065c7123188f4e1db8d3fdfc396
ipnn4 <nicolasip@student.apc.edu.ph>
2023-12-02 23:30:30 +0800

    updated

--- resources/views/initial_page/pets.blade.php
+++ resources/views/initial_page/pets.blade.php
@@ -1,17 +1,17 @@
 <x-guest-layout>
-    <div class=" flex justify-center align-center">
+    <div class=" flex justify-center">
         <div class="bg-red-900 w-full h-64">
             <h1>Banner siguro?</h1>
         </div>
     </div>
-    <div class="bg-gray-100 w-1/2 ml-auto mr-auto"> 
-        <div class="flex justify-center align-center pt-4">
+    <div class="bg-gray-100 max-w-fit ml-auto mr-auto"> 
+        <div class="flex justify-center items-center pt-4">
             <h1 class="font-bold text-4xl">Find your partner</h1>
         </div>
-        <div class="w-full px-4 pt-4 w-1/2">
-            <form class="w-full">   
+        <div class="px-4 pt-4 w-full">
+            <form class="flex lg:justify-end sm:justify-center">   
                 <label for="default-search" class="col-span-2 mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
+                <div class="relative w-1/2 ">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
@@ -22,7 +22,7 @@
                 </div>
             </form>
         </div>
-        <div class="grid grid-cols-4 px-6 gap-6 py-4">
+        <div class="lg:flex lg:justify-end px-6 py-4 gap-6 sm:grid sm:grid-cols-1">
             <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown hover <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                 </svg>
@@ -89,10 +89,12 @@
                 </li>
                 </ul>
             </div>
-            
+            <h5 class="flex items-center">
+                Sort by
+            </h5>
         </div>
-        <div class="flex justify-center align-center">
-            <div class="grid grid-cols-4 gap-6">
+        <div class="flex justify-center items-center px-6">
+            <div class="grid lg:grid-cols-4 gap-6 sm:grid grid-cols-1">
                 @foreach($pets as $pet)
                     <div class="object-cover h-72 w-52 p-2 border-black-100 border-2">
                         {{-- <h2>{{ $pet->pet_name }}</h2>

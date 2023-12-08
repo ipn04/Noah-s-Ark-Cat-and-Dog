bcd45df377c971823893e2cd4045048404612def
ipn04 <nicolasip@students.nu-fairview.edu.ph>
2023-12-08 22:37:30 +0800

    updated ulit

--- resources/views/initial_page/pets.blade.php
+++ resources/views/initial_page/pets.blade.php
@@ -1,12 +1,31 @@
 <x-guest-layout>
-    <div class=" flex justify-center">
-        <div class="bg-red-900 w-full h-72">
-            <h1>Banner siguro?</h1>
-        </div>
-    </div>
-    <div class="max-w-fit ml-auto mr-auto">
-        {{-- <div class="flex justify-end px-6 py-4">
-            <form role="search" class="flex items-center w-1/2">
+    <div class="bg-red-700">
+        <div class="mx-auto flex justify-between py-4  max-w-7xl">
+            
+            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+            </button>
+            
+            <!-- Dropdown menu -->
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                </li>
+                <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                </li>
+                <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                </li>
+                </ul>
+            </div>
+    
+            <form role="search" class="flex items-center w-1/3">
                 <label for="pet-search" class="sr-only">Search</label>
                 <div class="relative w-full">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -17,7 +36,9 @@
                     <input type="text" id="pet-search" placeholder="Search pet" name="search" required="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                 </div>
             </form>
-        </div>  --}}
+        </div> 
+    </div>
+    <div class="max-w-fit ml-auto mr-auto">
         <div class="px-6 py-4">
             <h1 class="font-bold text-2xl">List of Pets</h1>
             <hr class="h-0.5 mt-2 bg-gray-400 border-1">
@@ -25,7 +46,7 @@
       
        
         <div class="flex justify-center items-center px-6">
-            <div class="grid lg:grid-cols-4 gap-6 md:grid-cols-2 sm:grid grid-cols-1">
+            <div class="grid lg:grid-cols-5 gap-6 md:grid-cols-3 sm:grid grid-cols-1 max-w-7xl">
                 @foreach($pets as $pet)
                     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">

b23fdae6c1baf88f093ca4f63875b23d51a09584
ipn04 <nicolasip@students.nu-fairview.edu.ph>
2023-12-08 21:27:32 +0800

    pa check

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,6 +25,12 @@
                 {{ $slot }}
             </div>
         </div>
+        @elseif(Route::is('register')) 
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+            <div class="w-full sm:max-w-md px-6 py-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
+        </div>
         @elseif(Route::is('pets'))
             @include('navbar.main_navbar')
             <div class="mt-20">
@@ -36,7 +42,7 @@
             <div class="mt-20">
                 {{ $slot }}
             </div>
-
+        
         @else 
             @include('navbar.main_navbar')
             <div class="mt-20 flex justfify-center align-center w-full">

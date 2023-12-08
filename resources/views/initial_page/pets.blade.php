4bd0256d09652a3e31286ae5b4af0fa215c6fc10
ipn04 <nicolasip@students.nu-fairview.edu.ph>
2023-12-08 22:03:40 +0800

    sa home oage

--- resources/views/initial_page/pets.blade.php
+++ resources/views/initial_page/pets.blade.php
@@ -1,17 +1,31 @@
 <x-guest-layout>
     <div class=" flex justify-center">
-        <div class="bg-red-900 w-full h-64">
+        <div class="bg-red-900 w-full h-72">
             <h1>Banner siguro?</h1>
         </div>
     </div>
-    <div class="bg-gray-100 max-w-fit ml-auto mr-auto"> 
-        <div class="flex justify-center items-center pt-4">
-            <h1 class="font-bold text-4xl">Find your partner</h1>
+    <div class="max-w-fit ml-auto mr-auto">
+        {{-- <div class="flex justify-end px-6 py-4">
+            <form role="search" class="flex items-center w-1/2">
+                <label for="pet-search" class="sr-only">Search</label>
+                <div class="relative w-full">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
+                        </svg>
+                    </div>
+                    <input type="text" id="pet-search" placeholder="Search pet" name="search" required="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                </div>
+            </form>
+        </div>  --}}
+        <div class="px-6 py-4">
+            <h1 class="font-bold text-2xl">List of Pets</h1>
+            <hr class="h-0.5 mt-2 bg-gray-400 border-1">
         </div>
       
        
         <div class="flex justify-center items-center px-6">
-            <div class="grid lg:grid-cols-4 gap-6 sm:grid grid-cols-1">
+            <div class="grid lg:grid-cols-4 gap-6 md:grid-cols-2 sm:grid grid-cols-1">
                 @foreach($pets as $pet)
                     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">

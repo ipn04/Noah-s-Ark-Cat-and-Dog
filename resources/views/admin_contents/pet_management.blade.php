e5e1b3554d0eba545a482ac7ac393cc105d4c9b4
ipn04 <nicolasip@students.nu-fairview.edu.ph>
2023-12-10 12:57:21 +0800

    minor

--- resources/views/admin_contents/pet_management.blade.php
+++ resources/views/admin_contents/pet_management.blade.php
@@ -67,23 +67,22 @@
     <section class="sm:ml-64 mb-5 dark:bg-gray-900 p-2 antialiased">
         <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
             @if ($errors->any())
-                <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                    </svg>
-                    <span class="sr-only">Info</span>
-                    <div>
-                        <span class="font-medium">
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        </span>
-                    </div>
-                </div>
+                <script>
+                    var errorMessages = [];
+                    @foreach ($errors->all() as $error)
+                        errorMessages.push("{{ $error }}");
+                    @endforeach
+            
+                    // Check if there are error messages before showing the alert
+                    if (errorMessages.length > 0) {
+                        swal({
+                            title: "Error!",
+                            text: errorMessages.join('\n'), // Join error messages with line breaks
+                            type: "error",
+                            confirmButtonText: "Cool"
+                        });
+                    }
+                </script>
             @endif
 
             @if(session('pet_added'))

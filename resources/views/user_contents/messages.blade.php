67a5eb717de6e28d0e0b7e6a6c4c7db2adf6f987
ipnn4 <nicolasip@student.apc.edu.ph>
2023-12-13 09:51:57 +0800

    partial revisions

--- resources/views/user_contents/messages.blade.php
+++ resources/views/user_contents/messages.blade.php
@@ -38,11 +38,8 @@
                         <div class="col-span-2 sm:col-span-1 py-2">
                             <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                             <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                <option selected="">Select category</option>
-                                <option value="TV">TV/Monitors</option>
-                                <option value="PC">PC</option>
-                                <option value="GA">Gaming/Console</option>
-                                <option value="PH">Phones</option>
+                                <option selected="">Adoption</option>
+                                <option value="TV">Donation</option>
                             </select>
                         </div>
                         <div class="col-span-2 py-2">

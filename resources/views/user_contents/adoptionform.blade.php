526cff0e70169dcf3b0d1ab658bc890eca6360ed
Czarina Cuarez <cuarezcs@students.nu-fairview.edu.ph>
2023-12-27 22:46:10 +0800

    adoption form

--- resources/views/user_contents/adoptionform.blade.php
+++ resources/views/user_contents/adoptionform.blade.php
@@ -216,37 +216,36 @@
                 <div id="section3" class="hidden">
                     <h1 class="text-xl font-bold text-left">Fill out your answers down below</h1>
                     <p class = "font-bold">Part 3</p>
-                        <div class="px-4 py-6">
-                        <div id="image-preview" class="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
-                            <input id="upload" type="file" class="hidden" accept="image/*" />
-                            <label for="upload" class="cursor-pointer">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-700 mx-auto mb-4">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                            </svg>
-                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload picture</h5>
-                            <p class="font-normal text-sm text-gray-400 md:px-6">Choose photo size should be less than <b class="text-gray-600">2mb</b></p>
-                            <p class="font-normal text-sm text-gray-400 md:px-6">and should be in <b class="text-gray-600">JPG, PNG, or GIF</b> format.</p>
-                            <span id="filename" class="text-gray-500 bg-gray-200 z-50"></span>
-                            </label>
+                        <div class="px-4 py-3">
+                            <div id="image-preview" class="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
+                                <input id="upload" type="file" class="hidden" accept="image/*" />
+                                <label for="upload" class="cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-700 mx-auto mb-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                                </svg>
+                                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload your Valid ID</h5>
+                                <p class="font-normal text-sm text-gray-400 md:px-6">Choose photo size should be less than <b class="text-gray-600">2mb</b></p>
+                                <p class="font-normal text-sm text-gray-400 md:px-6">and should be in <b class="text-gray-600">JPG, PNG, or GIF</b> format.</p>
+                                <span id="filename" class="text-gray-500 bg-gray-200 z-50"></span>
+                                </label>
+                            </div>
                         </div>
 
-                        <div class="px-4 py-6">
+                        <div class="px-4 py-3">
                             <div id="image-preview2" class="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
                                 <input id="upload2" type="file" class="hidden" accept="image/*" />
                                 <label for="upload2" class="cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-700 mx-auto mb-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                                 </svg>
-                                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload picture</h5>
+                                <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload your Signature</h5>
                                 <p class="font-normal text-sm text-gray-400 md:px-6">Choose photo size should be less than <b class="text-gray-600">2mb</b></p>
                                 <p class="font-normal text-sm text-gray-400 md:px-6">and should be in <b class="text-gray-600">JPG, PNG, or GIF</b> format.</p>
                                 <span id="filename2" class="text-gray-500 bg-gray-200 z-50"></span>
                                 </label>
                             </div>
                         </div>
-                    
-                    </div>
-
+                        
                     <div class = "flex gap-2 mt-3">
                         <x-primary-button type="button" onclick="showSection(3, 2)" >Previous</x-primary-button>
                         <x-primary-button type="submit" >Submit</x-primary-button>

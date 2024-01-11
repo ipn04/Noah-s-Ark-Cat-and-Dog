8d10b7cc6f16dde1460b75fbe953dcbf5f4abbb6
ipnn4 <nicolasip@student.apc.edu.ph>
2024-01-12 05:22:45 +0800

    added accept button in scheduling visit and added partial volunteer progress

--- resources/views/admin_contents/schedule.blade.php
+++ resources/views/admin_contents/schedule.blade.php
@@ -529,6 +529,15 @@
                                                 d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
                                         </svg>
                                     </button>
+                                    <td class="items-center gap-1 hidden lg:table-cell">
+                                        <form method="POST" action="{{ route('update.schedule.status', ['id' => $visit->visit_id]) }}">
+                                            @csrf
+                                            @method('PUT')
+                                            <button type="submit" class="py-2 px-3 text-sm font-medium text-center text-white bg-cyan-400 hover:bg-cyan-600 rounded-lg shadow-md">
+                                                Update Schedule
+                                            </button>
+                                        </form>
+                                    </td>
                                 </td>    
                             </tr>
                         @endforeach

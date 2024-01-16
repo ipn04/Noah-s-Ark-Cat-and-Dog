b2ed82532ee5b8caea2d2665d92d94c4b819ba12
Czarina Cuarez <cuarezcs@students.nu-fairview.edu.ph>
2024-01-16 14:37:08 +0800

    updated adoption progress admin

--- resources/views/admin_contents/adoptionprogress.blade.php
+++ resources/views/admin_contents/adoptionprogress.blade.php
@@ -770,7 +770,7 @@
                 <!-- Modal header -->
                 <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                     <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        {{-- {{ $adoption->pet->pet_name . ' Details' }} </h3> --}}
+                        {{ $adoption->pet->pet_name . ' Details' }} </h3>
                         <button type="button"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                             data-modal-hide="pet-modal">

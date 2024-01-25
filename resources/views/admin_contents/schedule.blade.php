9e67a67a59ccf163ec14a2f6a7f667dc46d2f977
ipnn4 <nicolasip@student.apc.edu.ph>
2024-01-26 01:28:22 +0800

    slight changes lang sa admin schedule

--- resources/views/admin_contents/schedule.blade.php
+++ resources/views/admin_contents/schedule.blade.php
@@ -196,21 +196,21 @@
                                                             d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
                                                     </svg>
                                                 </button>
-                                                
-                                                @else
-                                                {{-- comment ko muna kase 'di pa tapos si volunteer progress' --}}
-                                                {{-- <a href="{{ route('admin.volunteer.progress', [$schedule->interview_id])}}"> --}}
-                                                {{-- <a href="{{ route('admin.volunteer.progress', ['userId' => $schedule->user_id, 'id' => $schedule->interview_id])}}"> --}}
-                                                    <button type="button"
-                                                        class="py-2 px-3 text-sm font-medium text-center text-white bg-cyan-400 hover:bg-cyan-600 rounded-lg shadow-md">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                            fill="currentColor" class="w-4 h-4 ">
-                                                            <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
-                                                            <path fill-rule="evenodd" clip-rule="evenodd"
-                                                                d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
-                                                        </svg>
-                                                    </button>
-                                                </a>
+                                            </a>
+                                            @elseif($schedule->interview_type == 'application_volunteer') 
+                                            {{-- comment ko muna kase 'di pa tapos si volunteer progress' --}}
+                                            {{-- <a href="{{ route('admin.volunteer.progress', [$schedule->interview_id])}}"> --}}
+                                            <a href="{{ route('admin.volunteer.progress', ['userId' => $schedule->user_id, 'id' => $schedule->interview_id])}}">
+                                                <button type="button"
+                                                    class="py-2 px-3 text-sm font-medium text-center text-white bg-cyan-400 hover:bg-cyan-600 rounded-lg shadow-md">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                                        fill="currentColor" class="w-4 h-4 ">
+                                                        <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
+                                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                                            d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 010-1.113zM17.25 12a5.25 5.25 0 11-10.5 0 5.25 5.25 0 0110.5 0z" />
+                                                    </svg>
+                                                </button>
+                                            </a>
                                             @endif
                                             @elseif($schedule->schedule_type == 'Pickup')
                                             {{-- <a href="{{ route('admin.adoptionprogress', [$schedule->adoption_id])}}"> --}}
